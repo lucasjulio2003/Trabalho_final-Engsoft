e0d10012aba0f62334f7031d4bddf181ba016b55
Raissaluna3026 <raissalunana@gmail.com>
2025-06-22 12:42:41 -0300

    getters e setters das classes emprestimo, usuario, professor, alunograd e etc

--- Usuario/AlunoPosGrad.java
+++ Usuario/AlunoPosGrad.java
@@ -3,13 +3,29 @@ package Usuario;
 import Emprestimo.RegraEmprestimoAlunoGraduacao;
 
 public class AlunoPosGrad extends Usuario{
-    public static final int LIMITE_EMPRESTIMO = 2;
-    public static final int PRAZO_DIAS = 1;
+    public static final int LIMITE_EMPRESTIMO = 3;
+    public static final int PRAZO_DIAS = 5;
     private RegraEmprestimoAlunoGraduacao regraEmprestimo;
     
     public AlunoPosGrad(String id, String nome){
         super(id,nome);
         this.regraEmprestimo = new RegraEmprestimoAlunoGraduacao();
+    }
+
+    public static int getLimiteEmprestimo() {
+        return LIMITE_EMPRESTIMO;
+    }
+
+    public static int getPrazoDias() {
+        return PRAZO_DIAS;
+    }
+
+    public RegraEmprestimoAlunoGraduacao getRegraEmprestimo() {
+        return regraEmprestimo;
+    }
+
+    public void setRegraEmprestimo(RegraEmprestimoAlunoGraduacao regraEmprestimo) {
+        this.regraEmprestimo = regraEmprestimo;
     }
 
     
--- Usuario/Usuario.java
+++ Usuario/Usuario.java
@@ -13,6 +13,30 @@ public abstract class Usuario {
         this.id = id;
         this.nome = nome;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmprestimos(List<Emprestimo> emprestimos) {
+        this.emprestimos = emprestimos;
+    }
+
+    public List<Emprestimo> getEmprestimos() {
+        return emprestimos;
+    }
 }
 
 
